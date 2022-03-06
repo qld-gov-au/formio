d02c9152ddbaeae6fbc8dafcdbd47355c7e9620a
Devon Chau <devonpis@gmail.com>
2022-03-06 11:16:58 +1000

    update entry point

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import * as components from "./components";
 import { buildOptions } from "./options/buildOptions";
 import { getComponents } from "./utils/getComponents";
 
-// loop through the component folder index.js and add components to Formio for renderer and builder
 if (Formio) {
+  // loop through the component folder index.js and add components to Formio for renderer and builder
   Formio.use({
     components: getComponents(components),
-    options: {
-      builder: buildOptions,
-    },
+    options: buildOptions,
   });
 } else {
   console.warn(
-    "qg-formio.js requires Formio object initiated. Please refer to qld-gov-au.github.io/formio/?path=/docs/welcome--page for usage."
+    "qg-formio.js requires Formio script included in this page. Please refer to qld-gov-au.github.io/formio/?path=/docs/welcome--page for usage."
   );
 }
