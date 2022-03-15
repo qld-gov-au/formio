a9b709f706b817f19a731a247b1ef339bdd08f89
Devon Chau <devonpis@gmail.com>
2022-03-15 11:49:38 +1000

    update webpack option

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "qg-formio.js",
-    library: "$",
+    library: "qgFormio",
     libraryTarget: "umd",
   },
   module: {
