226678fc77f06287ef1205b69f7761127d3ec2ad
Devon Chau <devonpis@gmail.com>
2022-03-15 11:54:49 +1000

    remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const ESLintPlugin = require("eslint-webpack-plugin");
-// const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
