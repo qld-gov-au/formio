887d89dd1129e5807082f5f0b4a0759ac53c917c
Devon Chau <devonpis@gmail.com>
2022-03-17 08:30:44 +1000

    update webpack and add binary assets

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "formio-qld-custom-components.min.js",
+    filename: "formio-qld.min.js",
     library: "FormioQld",
     libraryTarget: "umd",
   },
