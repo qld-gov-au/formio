1fee5652291727f9069e5f7f7a7d41a011d39078
Devon Chau <devonpis@gmail.com>
2022-03-30 07:32:55 +1000

    add webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require("path");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const UnminifiedWebpackPlugin = require("unminified-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: {
+    "formio-qld.min": path.resolve(__dirname, "src/index.js"),
+    "formio-loader.min": path.resolve(
+      __dirname,
+      "src/matrixHelpers/formioLoader/index.js"
+    ),
+    "formio-script.dev.min": path.resolve(
+      __dirname,
+      "src/matrixHelpers/formioScript/index.dev.js"
+    ),
+    "formio-script.prod.min": path.resolve(
+      __dirname,
+      "src/matrixHelpers/formioScript/index.prod.js"
+    ),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "formio-qld.min.js",
-    library: "FormioQld",
-    libraryTarget: "umd",
+    filename: "[name].js",
+    // library: "FormioQld",
+    // libraryTarget: "umd",
   },
   module: {
     rules: [
@@ -30,12 +45,24 @@ module.exports = {
   },
   plugins: [
     new ESLintPlugin(),
-    new CopyPlugin({
-      patterns: [{ from: "lib", info: { minimized: true } }],
+    new UnminifiedWebpackPlugin({
+      include: [/formio-.*$/],
+      exclude: [
+        // path.resolve(__dirname, "./lib/premium.min.js"),
+        // path.resolve(__dirname, "./node_modules"),
+        // path.resolve(__dirname, "./src"),
+        // /formio.full.*$/,
+        // /premium.*$/,
+        // /node_modules/,
+        // /lib/,
+      ],
+      // include: /src/.*/,
     }),
-    // new CopyPlugin({
-    //   patterns: [{ from: "src/assets" }],
-    // }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "./lib"), info: { minimized: true } },
+      ],
+    }),
   ],
   mode: "production",
   devtool: "source-map",
