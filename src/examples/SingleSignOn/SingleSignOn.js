df92d7799ecd8107c3dc308d88433a0e5e941310
william dutton <will.dutt@gmail.com>
2022-03-29 13:45:29 +1000

    make form be replaced on login instead of full page refresh

--- src/examples/SingleSignOn/SingleSignOn.js
+++ src/examples/SingleSignOn/SingleSignOn.js
@@ -1,35 +1,54 @@
 export const createSSoForm = () => {
   const div = document.createElement("div");
-  const oidcform = document.createElement("div");
-  oidcform.setAttribute("id", "oidc_form");
-  div.appendChild(oidcform);
-  const formio = document.createElement("div");
-  formio.setAttribute("id", "formio");
-  oidcform.appendChild(formio);
-  const spinner = document.createElement("img");
-  // <div id="oidc_form"><div id="formio"><img src="https://www.qld.gov.au/__data/assets/image/0019/126703/Spinner-1s-200px.png"></div></div>
-  spinner.setAttribute(
-    "src",
-    "https://www.qld.gov.au/__data/assets/image/0019/126703/Spinner-1s-200px.png"
-  );
-  formio.appendChild(spinner);
+
+  function createOidcFormDiv() {
+    const oidcform = document.createElement("div");
+    oidcform.setAttribute("id", "oidc_form");
+    div.appendChild(oidcform);
+    return oidcform;
+  }
+
+  const oidcform = createOidcFormDiv();
+
+  function createFormioDiv() {
+    const formio = document.createElement("div");
+    formio.setAttribute("id", "formio");
+    div.appendChild(formio);
+    return formio;
+  }
+
+  const formioDiv = createFormioDiv();
+
+  function appendSpinner(parentElement) {
+    const spinner = document.createElement("img");
+    // <div id="oidc_form"><div id="formio"><img src="https://www.qld.gov.au/__data/assets/image/0019/126703/Spinner-1s-200px.png"></div></div>
+    spinner.setAttribute(
+      "src",
+      "https://www.qld.gov.au/__data/assets/image/0019/126703/Spinner-1s-200px.png"
+    );
+    parentElement.appendChild(spinner);
+  }
 
   setTimeout(() => {
-    Formio.namespace = "auth";
+    Formio.namespace = "formio_ncwawujlwylhrfy";
     const formioProjectId = "ncwawujlwylhrfy"; // configure in squiz component
     const formioLoginFormId = "oidcsso"; // configure in squiz component
     const formioServiceFormId = "devauthform"; // configure in squiz component
     const formioBaseEndpoint = `https://api.forms.platforms.qld.gov.au/${formioProjectId}`;
     const formioLoginFormEndpoint = `${formioBaseEndpoint}/${formioLoginFormId}?live=1`;
     const formioServiceFormEndpoint = `${formioBaseEndpoint}/${formioServiceFormId}`;
-    const user = Formio.getUser();
-    console.info(user);
-    if (user) {
+    let user = null;
+    let oidcForm = null;
+    let realForm = null;
+
+    function realFormSetup() {
       console.info("Found user, redirecting to the form...");
-      Formio.createForm(
-        document.getElementById("oidc_form"),
+      realForm = Formio.createForm(
+        document.getElementById("formio"),
         formioServiceFormEndpoint
-      ).then(function postSetup(form) {
+      );
+
+      realForm.then(function postSetup(form) {
         window.form = form;
         // This section of code is the "Form Controller"
         form.on("submitDone", function submitDoneCleanup(submission) {
@@ -55,12 +74,15 @@ export const createSSoForm = () => {
             }
             window.location.href = logoutUrl + params;
           }
+
           setTimeout(afterTimeout, 500); // Logout after 500 seconds
         });
       });
-    } else {
+    }
+
+    function loginFormSetup() {
       console.info("No current user, rendering oidc sso form...");
-      const oidcForm = Formio.createForm(
+      oidcForm = Formio.createForm(
         document.getElementById("oidc_form"),
         formioLoginFormEndpoint
       );
@@ -72,15 +94,35 @@ export const createSSoForm = () => {
           Formio.currentUser().then((userDetails) => {
             // clean up URL paramters from submission or logout redirect
             console.info(userDetails);
-
-            const formioCleanURL = window.location.href.split("?")[0];
-
-            // trigger post-login form to load
-            window.location.href = formioCleanURL;
+            // const parent = document.getElementById("oidc_form").parentElement;
+            document.getElementById("oidc_form").remove();
+            // const oidcformNew = document.createElement("div");
+            // oidcformNew.setAttribute("id", "oidc_form");
+            // parent.appendChild(oidcform);
+            // eslint-disable-next-line no-use-before-define
+            pickForm();
+            // const formioCleanURL = window.location.href.split("?")[0];
+            //
+            // // trigger post-login form to load
+            // window.location.href = formioCleanURL;
           });
         });
       });
     }
+
+    function pickForm() {
+      user = Formio.getUser();
+      console.info(user);
+      if (user) {
+        appendSpinner(formioDiv);
+        realFormSetup();
+      } else {
+        appendSpinner(oidcform);
+        loginFormSetup();
+      }
+    }
+
+    pickForm();
   }, 100);
 
   return div;
