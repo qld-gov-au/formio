6e1464cf56aa9ba5510aa566320f841cf4ab0cdb
Devon Chau <devonpis@gmail.com>
2022-04-05 16:14:55 +1000

    remove scss to rely on swe

--- src/options/createForm.options.js
+++ src/options/createForm.options.js
@@ -7,6 +7,10 @@ module.exports = {
     showSubmit: false,
   },
   i18n: {
-    en: { pattern: "Must use the format shown" },
+    en: {
+      pattern: "Must use the format shown",
+      error:
+        '<h2><span class="fa fa-exclamation-triangle"></span> Please check your answers</h2>',
+    },
   },
 };
