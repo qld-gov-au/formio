bf1e7e6a7f24618ba8f5cc901d19bdae081db58f
Devon Chau <devonpis@gmail.com>
2022-04-05 10:45:12 +1000

    refactor form options

---
+++ src/options/createForm.options.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Turn off default buttons
+  buttonSettings: {
+    showCancel: false,
+    showPrevious: false,
+    showNext: false,
+    showSubmit: false,
+  },
+  i18n: {
+    en: { pattern: "Must use the format shown" },
+  },
+};
--- src/utils/createForm.js
+++ src/utils/createForm.js
@@ -1,4 +1,10 @@
-export const createForm = ({ form = {}, options = {}, controller }) => {
+import createFormOptions from "../options/createForm.options";
+
+export const createForm = ({
+  form = {},
+  options = createFormOptions,
+  controller,
+}) => {
   const div = document.createElement("div");
   div.className = "formio_container";
 
