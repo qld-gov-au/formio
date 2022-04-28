061f43fbef1ad68baac53830a4ab306086cc6dd7
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-04-28 10:18:45 +1000

    initial setup

--- src/examples/PdfDownload/PdfDownload.js
+++ src/examples/PdfDownload/PdfDownload.js
@@ -5,6 +5,10 @@ export function PdfDownload() {
   setTimeout(() => {
     FormioLoader.initFormio();
   });
+  // emulate Squiz Matrix render the formio container in asset #249261
+  // please note that in the template asset, i've add the ^htmlentities to render pdf-download-message
+  // ie. data-formio-pdf-download-message="%asset_metadata_pdf-download-message^htmlentities%"
+  // so the rich text value is htmlentities encoded and can be passed as data attribute with double quotes
   div.innerHTML = `
         <div id="formio" 
           class="qg-forms-v2"
@@ -13,6 +17,8 @@ export function PdfDownload() {
           data-formio-form-name="contactus" 
           data-formio-env-url="qol-formio-t-api.azurefd.net"
           data-formio-createForm-controller="customFn2"
+          data-formio-pdf-download="true"
+          data-formio-pdf-download-message="&lt;p class=&quot;m1&quot;&gt;Test message&lt;/p&gt;"
         ></div>
       `;
   return div;
