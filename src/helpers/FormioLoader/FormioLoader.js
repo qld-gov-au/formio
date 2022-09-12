135f1daf5f1a781f3c7c79e5d3d15c9ca874d422
devonpis <devonpis@gmail.com>
2022-09-12 14:29:14 +1000

    update FormioLoader (#94)
    - fix an issue in FormioLoader that would affect nested form implementation
    - update versions.txt

--- src/helpers/FormioLoader/FormioLoader.js
+++ src/helpers/FormioLoader/FormioLoader.js
@@ -16,8 +16,10 @@ const requestPluginHandler = (requestArgs, opts) => {
       requestArgs.opts.base = requestArgs.formio.options.base;
       requestArgs.opts.project = requestArgs.formio.options.project;
       requestArgs.opts.namespace = requestArgs.formio.options.namespace;
-      Formio.setProjectUrl(requestArgs.formio.options.project);
-      Formio.setBaseUrl(requestArgs.formio.options.base);
+      if (requestArgs.formio.options.project)
+        Formio.setProjectUrl(requestArgs.formio.options.project);
+      if (requestArgs.formio.options.base)
+        Formio.setBaseUrl(requestArgs.formio.options.base);
     }
   }
 
