daecb5cc49fe4288fa3992b5fd781345edc8b6df
Even Stensberg <evenstensberg@gmail.com>
2023-07-26 20:39:25 +0200

    fix: add context

--- .webpack/webpack.config.js
+++ .webpack/webpack.config.js
@@ -16,6 +16,7 @@ const getScriptConfig = (src) => {
 };
 
 module.exports = {
+  context: path.resolve(__dirname, ".."),
   entry: {
     "formio-qld.min": {
       import: path.resolve(__dirname, "src/index.js"),
